1b055a6dc93a6d9dd1443ce244295c87f7f03b30
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2023-07-28 17:31:24 -0300

    Fixed messages and suffix breaking

--- src/main/kotlin/io/lwcl/commands/BetterHomesCMD.kt
+++ src/main/kotlin/io/lwcl/commands/BetterHomesCMD.kt
@@ -2,7 +2,6 @@ package io.lwcl.commands
 
 import cloud.commandframework.annotations.*
 import io.lwcl.BetterHomes
-import io.lwcl.api.objects.ModernText
 import io.lwcl.menu.ListMenu
 import net.william278.huskhomes.user.OnlineUser
 import org.bukkit.command.CommandSender
@@ -63,7 +62,7 @@ class BetterHomesCMD(private val plugin: BetterHomes) {
     @CommandPermission("betterhomes.change.icon")
     fun onMaterial(
         commandSender: CommandSender,
-        @Argument(value = "home", suggestions = "homes") homeName: String,
+        @Argument(value = "home") homeName: String,
         @Argument(value = "material", suggestions = "materials") materialID: String,
     ) {
         if (commandSender is Player) {
@@ -78,7 +77,7 @@ class BetterHomesCMD(private val plugin: BetterHomes) {
     @CommandPermission("betterhomes.change.suffix")
     fun onSuffix(
         commandSender: CommandSender,
-        @Argument(value = "home", suggestions = "homes") homeName: String,
+        @Argument(value = "home") homeName: String,
         @Argument(value = "suffix") suffix: String,
     ) {
         if (commandSender is Player) {
@@ -91,18 +90,26 @@ class BetterHomesCMD(private val plugin: BetterHomes) {
 
     private fun changeSuffix(sender: CommandSender, owner: OnlineUser, homeName: String, suffix: String) {
         plugin.huskHomesAPI.editHomeMetaTags(owner, homeName) { tags ->
-            tags["betterhomes:suffix"] = ModernText.miniModernText(suffix).toLegacy()
+            tags["betterhomes:suffix"] = suffix
         }
-        sender.sendMessage(plugin.locale.getLocale("messages.other.change_material").toComponent())
+        sender.sendMessage(plugin.locale.getExpanded("messages.other.change_suffix", mutableMapOf(
+            "suffix" to suffix,
+            "home" to homeName,
+            "owner" to owner.username
+        )).toComponent())
     }
 
     private fun changeMaterial(sender: CommandSender, owner: OnlineUser, homeName: String, materialID: String) {
         val material = plugin.settings.getMaterialOrNull(materialID)
         if (material != null) {
             plugin.huskHomesAPI.editHomeMetaTags(owner, homeName) { tags ->
-                tags["betterhomes:icon"] = material.toString()
+                tags["betterhomes:icon"] = material.name
             }
-            sender.sendMessage(plugin.locale.getLocale("messages.other.change_material").toComponent())
+            sender.sendMessage(plugin.locale.getExpanded("messages.other.change_material", mutableMapOf(
+                "material" to material.name,
+                "home" to homeName,
+                "owner" to owner.username
+            )).toComponent())
         } else {
             sender.sendMessage(plugin.locale.getLocale("messages.other.invalid_material").toComponent())
         }
